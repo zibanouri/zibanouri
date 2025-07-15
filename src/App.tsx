dc24a755d569129497106fc99fa8690f270a0454
Ziba Nouri <zibanouri13621115@gmail.com>
2025-07-15 18:52:55 +0330

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hallo Ziba du bist toll...
+      </h1>
+    </>
+  );
+}
+
+export default App;
